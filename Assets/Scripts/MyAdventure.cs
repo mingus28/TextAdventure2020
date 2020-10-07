7112d600cbebe14d5fce6ee1f4cde9a5e5d5d4e2
Mingus de Geest <329869@student.mboutrecht.nl>
2020-10-07 13:50:14 +0200

    Eerste push

---
+++ Assets/Scripts/MyAdventure.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class MyAdventure : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        ShowMainMenu();
+    }
+    void ShowMainMenu()
+    {
+        Terminal.ClearScreen();
+        ScreenText("Welkom bij de game");
+        ScreenText("Have fun");
+        ScreenText(" ");
+        ScreenText("Typ start om te beginnen");
+
+    }
+    void OnUserInput(string input)
+    {
+        if (input == "start ")
+        {
+            StartIntro();
+            
+            {Terminal.WriteLine("Jij bent Leet!");}
+
+        }
+    }
+
+    void StartIntro()
+    {
+        Terminal.ClearScreen();
+        Terminal.WriteLine("Het was een koude donkere nacht");
+        Terminal.WriteLine("Een goede nacht om Fortnite te spelen");
+        Terminal.WriteLine("Druk op start om te beginnen");
+        
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    void ScreenText(string tekst)
+    {
+        Terminal.WriteLine(tekst);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+}
